db2bfc74ef2d0b4214205f80ce37c8fa959298f1
Lewis Ng'ang'a <flammylewis4@gmail.com>
2024-06-26 23:04:50 +0300

    learning nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css'
+import { lusitana } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      </html>
   );
 }
